2b2b41bfa488251bf57783e72f0c6c4ba78e5dc2
Phanthakan <phanthakan@kkumail.com>
2021-03-13 16:34:32 +0700

    update

--- js/map.js
+++ js/map.js
@@ -1,60 +1,29 @@
 let map;
-var position = { lat: 13.84786, lng: 100.604274 };
-// const image =
-//     "https://www.flaticon.com/svg/vstatic/svg/1141/1141121.svg?token=exp=1615609037~hmac=e4112a99bb2d5e52fd7ccfc5f96699f9";
+var locations = [
+  ["โรงพยาบาลศรีนครินทร์", 16.4680574, 102.8300541],
+  ["โรงพยาบาลศูนย์ขอนแก่น", 16.4291172, 102.8491256],
+  ["โรงพยาบาลราชพฤกษ์", 16.4488145, 102.8302752],
+];
+
+var position = { lat: 16.4741193, lng: 102.8139075 };
+const image =
+  "https://www.flaticon.com/svg/vstatic/svg/1141/1141121.svg?token=exp=1615609037~hmac=e4112a99bb2d5e52fd7ccfc5f96699f9";
+
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: position,
     zoom: 15,
   });
-  infoWindow = new google.maps.InfoWindow();
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        infoWindow.setPosition(pos);
-        infoWindow.setContent("คุณอยู่ที่นี่");
-        infoWindow.open(map);
-        map.setCenter(pos);
-        var marker = new google.maps.Marker({
-          position: pos,
-          map: map,
-          // icon: image, 
-          
-        });
-      },
-      () => {
-        handleLocationError(true, infoWindow, map.getCenter());
-      }
-    );
-  } else {
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-      browserHasGeolocation
-        ? "Error: The Geolocation service failed."
-        : "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
-  }
 
   var marker = new google.maps.Marker({
-    position: pos,
-    map: map,
+    position:position,
+    map:map
+  })
+  
+  var infoWindow = new google.maps.InfoWindow({
+    content: '<div style="font-size: 15px;">sasdsaddfsdf</div>'
   });
 
-  var infowindow = new google.maps.InfoWindow({
-    content: "Hello World!",
+  google.maps.event.addListener(marker, 'click', function(){
+    infoWindow.open(map, marker)
   });
-
-  google.maps.event.addListener(marker, "click", function () {
-    infowindow.open(map, marker);
-  });
-  var marker, i, info;
-}
